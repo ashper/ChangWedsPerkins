4b942ca26e41c49d25f2baa7af03331454bc83b0
Ashley Perkins <ashley.perkins@juliusbaer.com>
2025-01-26 12:16:59 +0800

    Adding CNAME to publish command

--- src/Body.tsx
+++ src/Body.tsx
@@ -29,7 +29,7 @@ function CustomTabPanel(props: TabPanelProps) {
 function Body() {
   const [value, setValue] = useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
