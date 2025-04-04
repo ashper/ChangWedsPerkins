c938994cab93f9144604387f7ec291d46a87ad02
Ashley Perkins <ashley.perkins@juliusbaer.com>
2025-04-04 17:31:22 +0800

    Moving to React 19

--- src/Mobile/RSVP.tsx
+++ src/Mobile/RSVP.tsx
@@ -12,32 +12,20 @@ import {
   Stack,
   TextField,
 } from "@mui/material";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { useState } from "react";
 
 function RSVP() {
-  const [form, setForm] = useState<RSVPData>(defaultData);
+  const [attending, setAttending] = useState("true");
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setForm((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleChangeSelect = (event: SelectChangeEvent<string>) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setForm((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  function handleSubmit(formData: FormData) {
+    const formValues = Object.fromEntries(formData);
     fetch("https://www.formbackend.com/f/8abd8f07b64133a2", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: JSON.stringify(form),
+      body: JSON.stringify(formValues),
     })
       .then((response) => {
         if (response.status === 422) {
@@ -50,105 +38,50 @@ function RSVP() {
       })
       .then(() => {
         // You can even use `data` here. Access `data.submission_text`, `data.values` etc.
-        alert(JSON.stringify(form));
+        alert(JSON.stringify(formValues));
       })
       .catch((error) => {
         console.error("Error:", error);
       });
+  }
+
+  const handleAttendanceChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setAttending((event.target as HTMLInputElement).value);
   };
 
   return (
     <>
       <p>We need to put some text here to intro the RSVP form</p>
-      <form onSubmit={handleSubmit}>
+      <form action={handleSubmit}>
         <Stack spacing={2}>
-          <TextField
-            label="Name"
-            name="Name"
-            value={form.Name}
-            onChange={handleChange}
-          ></TextField>
+          <TextField label="Name" name="Name"></TextField>
           <FormControl>
             <FormLabel>Attendance</FormLabel>
             <RadioGroup
               name="Attending"
-              value={form.Attending}
-              onChange={handleChange}
+              value={attending}
+              onChange={handleAttendanceChange}
             >
               <FormControlLabel
-                value={true}
+                value="true"
                 control={<Radio />}
                 label="I will be attending"
               />
               <FormControlLabel
-                value={false}
+                value="false"
                 control={<Radio />}
                 label="I am unable to attend"
               />
             </RadioGroup>
           </FormControl>
 
-          <FormControl fullWidth>
-            <InputLabel>Number of Guests</InputLabel>
-            <Select
-              name="NumberOfGuests"
-              value={form.NumberOfGuests}
-              label="Number of Guests"
-              onChange={handleChangeSelect}
-            >
-              <MenuItem value="1">1</MenuItem>
-              <MenuItem value="2">2</MenuItem>
-              <MenuItem value="3">3</MenuItem>
-              <MenuItem value="4">4</MenuItem>
-              <MenuItem value="5">5</MenuItem>
-              <MenuItem value="6">6</MenuItem>
-              <MenuItem value="7">7</MenuItem>
-              <MenuItem value="8">8</MenuItem>
-              <MenuItem value="9">9</MenuItem>
-              <MenuItem value="10">10</MenuItem>
-            </Select>
-          </FormControl>
-
-          <TextField
-            label="Dietary Requirements"
-            name="DietaryRequirements"
-            value={form.DietaryRequirements}
-            onChange={handleChange}
-            multiline
-            minRows={2}
-            maxRows={4}
-          ></TextField>
-
-          <TextField
-            label="Accessibility Requirements"
-            name="AccessibilityRequirements"
-            value={form.AccessibilityRequirements}
-            onChange={handleChange}
-            multiline
-            minRows={2}
-            maxRows={4}
-          ></TextField>
-
-          <FormControl fullWidth>
-            <InputLabel>Preferred Contact Method</InputLabel>
-            <Select
-              name="ContactPreference"
-              value={form.ContactPreference}
-              label="Age"
-              onChange={handleChangeSelect}
-            >
-              <MenuItem value="Whatsapp">Whatsapp</MenuItem>
-              <MenuItem value="Phone">Phone</MenuItem>
-              <MenuItem value="Email">Email</MenuItem>
-            </Select>
-          </FormControl>
-
-          <TextField
-            label={form.ContactPreference}
-            name="ContactValue"
-            value={form.ContactValue}
-            onChange={handleChange}
-          ></TextField>
+          {attending === "true" ? (
+            <MoreDetails></MoreDetails>
+          ) : (
+            <SorryToHear></SorryToHear>
+          )}
 
           <Button type="submit" variant="outlined">
             RSVP
@@ -157,26 +90,82 @@ function RSVP() {
       </form>
     </>
   );
+
+  function SorryToHear() {
+    return (
+      <>
+        <p>We're sorry to hear you're not able to attend.</p>
+        <p>Thank you for letting us know.</p>
+      </>
+    );
+  }
+
+  function MoreDetails() {
+    const [numberOfGuests, setNumberOfGuests] = useState("1");
+
+    const handleNumberOfGuestsChange = (event: SelectChangeEvent) => {
+      setNumberOfGuests(event.target.value as string);
+    };
+    return (
+      <>
+        <FormControl fullWidth>
+          <InputLabel>Number of Guests</InputLabel>
+          <Select
+            name="NumberOfGuests"
+            label="Number of Guests"
+            value={numberOfGuests}
+            onChange={handleNumberOfGuestsChange}
+          >
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={6}>6</MenuItem>
+            <MenuItem value={7}>7</MenuItem>
+            <MenuItem value={8}>8</MenuItem>
+            <MenuItem value={9}>9</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+          </Select>
+        </FormControl>
+
+        {Number(numberOfGuests) > 1 &&
+          new Array(Number(numberOfGuests) - 1)
+            .fill("")
+            .map((_, index) => (
+              <TextField
+                key={index}
+                label={"Guest " + (index + 1)}
+                name={"ExtraGuest" + (index + 1)}
+              ></TextField>
+            ))}
+
+        <TextField
+          label="Dietary Requirements"
+          name="DietaryRequirements"
+          multiline
+          minRows={2}
+          maxRows={4}
+        ></TextField>
+
+        <TextField label="Whatsapp" name="Whatsapp" type="tel"></TextField>
+
+        <TextField label="Email" name="Email" type="email"></TextField>
+
+        <FormControl fullWidth>
+          <InputLabel>Preferred Contact Method</InputLabel>
+          <Select
+            name="ContactPreference"
+            label="Preferred Contact Method"
+            defaultValue={"Whatsapp"}
+          >
+            <MenuItem value="Whatsapp">Whatsapp</MenuItem>
+            <MenuItem value="Email">Email</MenuItem>
+          </Select>
+        </FormControl>
+      </>
+    );
+  }
 }
 
 export default RSVP;
-
-const defaultData: RSVPData = {
-  Attending: true,
-  Name: "",
-  NumberOfGuests: "1",
-  DietaryRequirements: "",
-  ContactPreference: "Whatsapp",
-  ContactValue: "",
-  AccessibilityRequirements: "",
-};
-
-export interface RSVPData {
-  Attending: boolean;
-  Name: string;
-  NumberOfGuests: string;
-  DietaryRequirements: string;
-  ContactPreference: "Whatsapp" | "Phone" | "Email";
-  ContactValue: string;
-  AccessibilityRequirements: string;
-}
